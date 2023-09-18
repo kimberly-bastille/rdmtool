40e538d1c4b3e5dca3e9aada1bad239f33acc290
kimberly-bastille <kimberly.bastille@noaa.gov>
2023-09-18 17:38:00 -0400

    add required packages install

---
+++ R/required_packages.R
@@ -0,0 +1,14 @@
+## Required Packages
+
+install.packages("shiny")
+install.packages("shinyjs")
+install.packages("shinywidgets")
+install.packages("magrittr")
+install.packages("readr")
+install.packages("here")
+install.packages("dplyr")
+install.packages("tidyr")
+install.packages("stringr")
+install.packages("lubridate")
+install.packages("tibble")
+install.packages("data.table")
